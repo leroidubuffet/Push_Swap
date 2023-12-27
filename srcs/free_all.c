650b1fd3cf6e29fda65980b9c295ba8c916c5987
Yago Bolívar <mr.bijou@gmail.com>
2023-12-27 19:42:42 +0100

    add ft_clear_and_exit

--- srcs/free_all.c
+++ srcs/free_all.c
@@ -6,7 +6,7 @@
 /*   By: airyago <airyago@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/26 14:59:02 by airyago           #+#    #+#             */
-/*   Updated: 2023/12/21 18:02:43 by airyago          ###   ########.fr       */
+/*   Updated: 2023/12/27 19:42:11 by airyago          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,15 @@ void	ft_free(t_stacks *stacks)
 	free(stacks->values);
 	free(stacks->moves);
 	free(stacks->best);
+}
+
+/**
+ * Clears nodes of stack_a and exits the program.
+ *
+ * @param stacks - Container holding both stacks.
+ */
+void ft_clear_and_exit(t_stacks *stacks)
+{
+	ft_clearnodes(&stacks->head_a);
+	exit(0);
 }
