2ce629a9febdbbf56d8c17f1f0343534c4a643a1
Yago Bolívar <mr.bijou@gmail.com>
2023-12-27 21:00:54 +0100

    secutiry checks

--- srcs/check_max_min_b.c
+++ srcs/check_max_min_b.c
@@ -14,10 +14,29 @@
 
 void	ft_check_limits_b(t_stacks *stacks)
 {
+	if (!stacks)
+		return ;
 	if (stacks->head_b != NULL)
 	{
 		ft_check_max_b(stacks, stacks->head_b);
 		ft_check_min_b(stacks, stacks->head_b);
+	}
+}
+
+void	ft_check_max_b(t_stacks *stacks, t_stack *head_b)
+{
+	int	i;
+	int	size;
+
+	i = 0;
+	size = ft_listsize(head_b);
+	stacks->values->max_b = head_b->data;
+	while (i < size)
+	{
+		if (stacks->values->max_b < head_b->data)
+			stacks->values->max_b = head_b->data;
+		head_b = head_b->next;
+		i++;
 	}
 }
 
@@ -38,19 +57,3 @@ void	ft_check_min_b(t_stacks *stacks, t_stack *head_b)
 	}
 }
 
-void	ft_check_max_b(t_stacks *stacks, t_stack *head_b)
-{
-	int	i;
-	int	size;
-
-	i = 0;
-	size = ft_listsize(head_b);
-	stacks->values->max_b = head_b->data;
-	while (i < size)
-	{
-		if (stacks->values->max_b < head_b->data)
-			stacks->values->max_b = head_b->data;
-		head_b = head_b->next;
-		i++;
-	}
-}
