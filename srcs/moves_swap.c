84d5c7de3a48af0d2252d99360e6b101691bf6fd
Yago Bolívar <mr.bijou@gmail.com>
2023-12-28 18:29:38 +0100

    new moves_swap.c simplified

--- srcs/moves_swap.c
+++ srcs/moves_swap.c
@@ -6,56 +6,73 @@
 /*   By: airyago <airyago@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/19 16:35:04 by ybolivar          #+#    #+#             */
-/*   Updated: 2023/12/28 16:48:17 by airyago          ###   ########.fr       */
+/*   Updated: 2023/12/28 18:29:04 by airyago          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ft_swap(t_stacks *stacks, char ch)
+/**
+ * Determines and executes the swap operation on the specified stack or both stacks simultaneously.
+ * It swaps the first two elements at the top of the stack(s) if they exist.
+ *
+ * @param stacks - A pointer to the struct containing both stack A and stack B.
+ * @param ch - A character indicating the target stack ('a' for stack A, 'b' for stack B, 's' for both stacks).
+ */
+void ft_swap(t_stacks *stacks, char ch)
 {
-	t_stack_a	*head_a;
-	t_stack_b	*head_b;
-
-	head_a = stacks->head_a;
-	head_b = stacks->head_b;
 	if (ch == 'a')
-		ft_swap_a(head_a, 1);
-	else if (ch == 'b')
+		ft_swap_a(stacks->head_a, 1);
+	else if (ch == 'b' && stacks->head_b && stacks->head_b->next)
+		ft_swap_b(stacks->head_b, 1);
+	else if (ch == 's' && stacks->head_a && stacks->head_a->next && stacks->head_b && stacks->head_b->next)
 	{
-		if (head_b == NULL || head_b->next == NULL)
-			return ;
-		ft_swap_b(head_b, 1);
-	}
-	else
-	{
-		if (head_a == NULL || head_a->next == NULL || head_b == NULL
-			|| head_b->next == NULL)
-			return ;
-		ft_swap_a(head_a, 0);
-		ft_swap_b(head_b, 0);
+		ft_swap_a(stacks->head_a, 0);
+		ft_swap_b(stacks->head_b, 0);
 		ft_putstr("ss\n");
 	}
 }
 
-void	ft_swap_a(t_stack_a *head_a, int print)
+/**
+ * Swaps the first two elements of stack A.
+ * If there are at least two elements, their values are swapped.
+ * Records the operation by printing "sa" if the print flag is set.
+ *
+ * @param head_a - The top node of stack A.
+ * @param print - Flag indicating whether to print the operation.
+ */
+void ft_swap_a(t_stack_a *head_a, int print)
 {
-	int	tmp;
+	int tmp;
 
-	tmp = head_a->content;
-	head_a->content = head_a->next->content;
-	head_a->next->content = tmp;
-	if (print != 0)
-		ft_putstr("sa\n");
+	if (head_a && head_a->next)
+	{
+		tmp = head_a->content;
+		head_a->content = head_a->next->content;
+		head_a->next->content = tmp;
+		if (print)
+			ft_putstr("sa\n");
+	}
 }
 
-void	ft_swap_b(t_stack_b *head_b, int print)
+/**
+ * Swaps the first two elements of stack B.
+ * If there are at least two elements, their values are swapped.
+ * Records the operation by printing "sb" if the print flag is set.
+ *
+ * @param head_b - The top node of stack B.
+ * @param print - Flag indicating whether to print the operation.
+ */
+void ft_swap_b(t_stack_b *head_b, int print)
 {
-	int	tmp;
+	int tmp;
 
-	tmp = head_b->content;
-	head_b->content = head_b->next->content;
-	head_b->next->content = tmp;
-	if (print != 0)
-		ft_putstr("sb\n");
+	if (head_b && head_b->next)
+	{
+		tmp = head_b->content;
+		head_b->content = head_b->next->content;
+		head_b->next->content = tmp;
+		if (print)
+			ft_putstr("sb\n");
+	}
 }
