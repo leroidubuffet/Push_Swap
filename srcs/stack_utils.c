a6a4e161c5c3ab46c8157038eca07fc43dffd22f
Yago Bolívar <mr.bijou@gmail.com>
2023-12-29 13:07:25 +0100

    new stack_utils.c with initialize and free stacks functions

--- srcs/stack_utils.c
+++ srcs/stack_utils.c
@@ -1,16 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   free_all.c                                         :+:      :+:    :+:   */
+/*   stack_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: airyago <airyago@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/26 14:59:02 by ybolivar          #+#    #+#             */
-/*   Updated: 2023/12/29 13:00:05 by airyago          ###   ########.fr       */
+/*   Updated: 2023/12/29 13:06:50 by airyago          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+/**
+ * Initializes the stacks for the push_swap algorithm.
+ * It sets up the stack 'a' with the input arguments and ensures stack 'b'
+ * is empty.
+ *
+ * @param stacks - A pointer to the structure containing both stack
+ * 'a' and 'b'.
+ * @param argc - The count of command-line arguments, representing
+ * the numbers to sort.
+ * @param argv - The array of command-line arguments, representing
+ * the numbers to sort.
+ */
+void	ft_initialize_stacks(t_stacks *stacks, int argc, char **argv)
+{
+	if (!stacks)
+		return ;
+	stacks->head_a = ft_create_list(argc, argv);
+	stacks->head_b = NULL;
+}
 
 /**
  * Frees all dynamically allocated memory within the t_stacks structure.
@@ -21,8 +41,9 @@
  * @param stacks - Pointer to the t_stacks structure containing
  * all the allocated elements.
  */
-void	ft_free(t_stacks *stacks)
-{	if (!stacks)
+void	ft_free_stacks(t_stacks *stacks)
+{
+	if (!stacks)
 		return ;
 	ft_clearnodes_a(&stacks->head_a);
 	ft_clearnodes_b(&stacks->head_b);
