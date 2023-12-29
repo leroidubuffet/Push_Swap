e1e2841df90a2bda2313c7f6d937c5358ca29e61
Yago Bolívar <mr.bijou@gmail.com>
2023-12-29 12:43:42 +0100

    consolidate ft_check_order in sorting.c and remove check_list.c

--- srcs/sorting.c
+++ srcs/sorting.c
@@ -6,11 +6,37 @@
 /*   By: airyago <airyago@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 15:59:54 by ybolivar          #+#    #+#             */
-/*   Updated: 2023/12/29 12:33:29 by airyago          ###   ########.fr       */
+/*   Updated: 2023/12/29 12:42:29 by airyago          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+/**
+ * Checks if the elements in stack 'a' are in ascending order.
+ *
+ * @param stacks - The container holding both stacks.
+ * @return Returns 1 if the stack is in order, 0 otherwise.
+ */
+static bool ft_check_order(t_stacks *stacks)
+{
+	t_stack_a	*current_node;
+	int			last_value;
+
+	if (!stacks || !stacks->head_a || !stacks->head_a->next)
+		return (1);
+	current_node = stacks->head_a;
+	last_value = current_node->content;
+	current_node = current_node->next;
+	while (current_node)
+	{
+		if (last_value > current_node->content)
+			return (0);
+		last_value = current_node->content;
+		current_node = current_node->next;
+	}
+	return (1);
+}
 
 /**
  * Sorts the elements in stack_a of the given t_stacks.
