8becdb07530e6ae968dd260225138c50a24d39eb
Yago Bolívar <mr.bijou@gmail.com>
2023-12-29 16:33:20 +0100

    norminette

--- srcs/limits_to_top.c
+++ srcs/limits_to_top.c
@@ -6,7 +6,7 @@
 /*   By: airyago <airyago@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 15:43:17 by ybolivar          #+#    #+#             */
-/*   Updated: 2023/12/28 20:26:40 by airyago          ###   ########.fr       */
+/*   Updated: 2023/12/29 16:29:44 by airyago          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,23 +20,18 @@
  */
 static void ft_move_max_to_top(t_stacks *stacks)
 {
-    // Rotate stack A until the maximum element is at the top.
-    while (stacks->moves->ra > 0)
-    {
-        ft_rotate(stacks, 'a');
-        stacks->moves->ra--;
-    }
-
-    // Reverse rotate stack A until the maximum element is at the top.
-    while (stacks->moves->rra > 0)
-    {
-        ft_rev_rotate(stacks, 'a');
-        stacks->moves->rra--;
-    }
-
-    // Push the maximum element to stack B and then rotate to position it properly.
-    ft_push(stacks, 'a');
-    ft_rotate(stacks, 'a');
+	while (stacks->moves->ra > 0)
+	{
+		ft_rotate(stacks, 'a');
+		stacks->moves->ra--;
+	}
+	while (stacks->moves->rra > 0)
+	{
+		ft_rev_rotate(stacks, 'a');
+		stacks->moves->rra--;
+	}
+	ft_push(stacks, 'a');
+	ft_rotate(stacks, 'a');
 }
 
 /**
@@ -48,33 +43,31 @@ static void ft_move_max_to_top(t_stacks *stacks)
  */
 void ft_calc_max_to_top(t_stacks *stacks)
 {
-    int i;
-    int size;
+	int i;
+	int size;
 
-    stacks->moves->ra = 0;
-    stacks->moves->rra = 0;
-    if (ft_listlast_a(stacks->head_a)->content != stacks->values->max_a)
-    {
-        i = ft_find_index_stack_a(stacks, stacks->values->max_a);
-        size = ft_listsize_a(stacks->head_a);
-
-        // Determine the most efficient direction to rotate.
-        if (size % 2 == 0)
-        {
-            if (i + 1 > size / 2)
-                stacks->moves->rra = size - i - 1;
-            else
-                stacks->moves->ra = i + 1;
-        }
-        else
-        {
-            if (i > size / 2)
-                stacks->moves->rra = size - i - 1;
-            else
-                stacks->moves->ra = i + 1;
-        }
-    }
-    ft_move_max_to_top(stacks);
+	stacks->moves->ra = 0;
+	stacks->moves->rra = 0;
+	if (ft_listlast_a(stacks->head_a)->content != stacks->values->max_a)
+	{
+		i = ft_find_index_stack_a(stacks, stacks->values->max_a);
+		size = ft_listsize_a(stacks->head_a);
+		if (size % 2 == 0)
+		{
+			if (i + 1 > size / 2)
+				stacks->moves->rra = size - i - 1;
+			else
+				stacks->moves->ra = i + 1;
+		}
+		else
+		{
+			if (i > size / 2)
+				stacks->moves->rra = size - i - 1;
+			else
+				stacks->moves->ra = i + 1;
+		}
+	}
+	ft_move_max_to_top(stacks);
 }
 
 /**
@@ -85,20 +78,18 @@ void ft_calc_max_to_top(t_stacks *stacks)
  */
 static void ft_move_min_to_top(t_stacks *stacks)
 {
-    // Perform the rotations or reverse rotations as calculated.
-    while (stacks->moves->ra > 0)
-    {
-        ft_rotate(stacks, 'a');
-        stacks->moves->ra--;
-    }
-    while (stacks->moves->rra > 0)
-    {
-        ft_rev_rotate(stacks, 'a');
-        stacks->moves->rra--;
-    }
-
-    // Push the now-top minimum element from stack A to stack B.
-    ft_push(stacks, 'a');
+	// Perform the rotations or reverse rotations as calculated.
+	while (stacks->moves->ra > 0)
+	{
+		ft_rotate(stacks, 'a');
+		stacks->moves->ra--;
+	}
+	while (stacks->moves->rra > 0)
+	{
+		ft_rev_rotate(stacks, 'a');
+		stacks->moves->rra--;
+	}
+	ft_push(stacks, 'a');
 }
 
 /**
@@ -109,31 +100,20 @@ static void ft_move_min_to_top(t_stacks *stacks)
  */
 void ft_calc_min_to_top(t_stacks *stacks)
 {
-    int index;
-    int stack_size;
+	int	index;
+	int	stack_size;
 
-    // Reset the count for rotations and reverse rotations.
-    stacks->moves->ra = 0;
-    stacks->moves->rra = 0;
-
-    // Only proceed if the minimum element isn't already at the top of stack A.
-    if (stacks->head_a->content != stacks->values->min_a)
-    {
-        index = ft_find_index_stack_a(stacks, stacks->values->min_a);
-        stack_size = ft_listsize_a(stacks->head_a);
-
-        // Determine rotation direction based on the element's position.
-        if ((stack_size % 2 == 0 && index + 1 > stack_size / 2) ||
-            (index > stack_size / 2))
-        {
-            stacks->moves->rra = stack_size - index;
-        }
-        else
-        {
-            stacks->moves->ra = index;
-        }
-    }
-
-    // Execute the moves to position the minimum element at the top.
-    ft_move_min_to_top(stacks);
+	stacks->moves->ra = 0;
+	stacks->moves->rra = 0;
+	if (stacks->head_a->content != stacks->values->min_a)
+	{
+		index = ft_find_index_stack_a(stacks, stacks->values->min_a);
+		stack_size = ft_listsize_a(stacks->head_a);
+		if ((stack_size % 2 == 0 && index + 1 > stack_size / 2) ||
+			(index > stack_size / 2))
+			stacks->moves->rra = stack_size - index;
+		else
+			stacks->moves->ra = index;
+	}
+	ft_move_min_to_top(stacks);
 }
